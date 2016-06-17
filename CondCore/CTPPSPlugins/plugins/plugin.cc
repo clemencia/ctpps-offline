ac8ba7835ac60f46415d8d9a2ee547ac1fb6e33f
Clemencia <clemencia.mora.herrera@cern.ch>
2016-06-17 21:11:08 +0200

    ctppsobjects plugins

---
+++ CondCore/CTPPSPlugins/plugins/plugin.cc
@@ -0,0 +1,5 @@
+#include "CondCore/ESSources/interface/registration_macros.h"
+#include "CondFormats/CTPPSObjects/interface/CTPPSRPPositions.h"
+#include "CondFormats/DataRecord/interface/CTPPSRPPositionsRcd.h"
+
+REGISTER_PLUGIN(CTPPSRPPositionsRcd,CTPPSRPPositions);
