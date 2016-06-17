2cfa5f15b2c606d9ae0226f2d64ad0566920f6f7
Clemencia <clemencia.mora.herrera@cern.ch>
2016-06-17 20:25:49 +0200

    add object in registration macro and class dictionary files

---
+++ CondFormats/CTPPSObjects/src/classes.h
@@ -0,0 +1,10 @@
+#include "CondFormats/CTPPSObjects/src/headers.h"
+
+namespace CondFormats_CTPPSObjects{
+  struct dictionary {
+    std::map<unsigned int,CTPPSRPPosition> myposmap;
+
+
+  };
+  
+}
---
+++ CondFormats/CTPPSObjects/src/headers.h
@@ -0,0 +1,1 @@
+#include "CondFormats/CTPPSObjects/interface/CTPPSRPPositions.h"
